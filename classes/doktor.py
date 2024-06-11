d7a0f4a90cdfe8a302eba38c3c799d8e543358e8
ildEmre <huseyinemreildeniz@gmail.com>
2024-06-12 02:37:04 +0300

    Doktor sınıfı oluşturuldu

---
+++ classes/doktor.py
@@ -0,0 +1,23 @@
+from classes.personel import *
+class Doktor(Personel):
+    def __init__(self, personelNo, ad, soyad, departman, maas, uzmanlik, deneyimYili, hastane):
+        super().__init__(self, personelNo, ad, soyad, departman, maas)
+        self.uzmanlik = uzmanlik
+        self.deneyimYili = deneyimYili
+        self.hastane = hastane
+    def getUzmanlik(self):
+        return self.uzmanlik
+    def setUzmanlik(self, uzmanlik):
+        self.uzmanlik = uzmanlik
+    def getDeneyimYili(self):
+        return self.deneyimYili
+    def setDeneyimYili(self, deneyimYili):
+        self.deneyimYili = deneyimYili
+    def getHastane(self):
+        return self.hastane
+    def setHastane(self, hastane):
+        self.hastane = hastane
+    def __str__(self):
+        return super().__str__() + "\nUzmanlik: ", self.uzmanlik, "\nDeneyim yili: ", self.deneyimYili, "\nHastane: ", self.hastane
+    def maasArttir(self, oran):
+        self.maas += self.maas * oran/100
