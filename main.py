709878c244d088e999a1169ec38f6085eefa60c0
ildEmre <huseyinemreildeniz@gmail.com>
2024-06-12 03:07:55 +0300

    dataframe oluşturuldu ve try/except eklendi

---
+++ main.py
@@ -0,0 +1,25 @@
+try:
+    from classes import *
+except:
+    print("Dosya yüklenirken hata oluştu. Tekrar deneyiniz.")
+try:
+    from pandas import DataFrame, Series
+except:
+    print("Dosya yüklenirken hata oluştu. Tekrar deneyiniz.")
+
+data = DataFrame({
+    "Personel No":  [],
+    "Ad":           [],
+    "Soyad":        [],
+    "Departman":    [],
+    "Maas":         [],
+    "Uzmanlik":     [],
+    "Deneyim Yili": [],
+    "Hastane":      [],
+    "Calisma Saati":[],
+    "Sertifika":    [],
+    "Hasta No" :    [],
+    "Dogum Tarihi" :[],
+    "Hastalik" :    [],
+    "Tedavi" :      []
+})
